b74b6a35b4ff56c793dd3d88c73d6f67928b5634
VR2011 <64109448+VR2011@users.noreply.github.com>
2020-09-04 15:34:34 -0400

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -9,9 +9,9 @@ class SlingShot{
             length: 10
         }
         //Loads the image for each part.
-        this.sling1 = loadImage('sprites/sling1.png');
-        this.sling2 = loadImage('sprites/sling2.png');
-        this.sling3 = loadImage('sprites/sling3.png');
+        this.sling1 = loadImage('sling1.png');
+        this.sling2 = loadImage('sling2.png');
+        this.sling3 = loadImage('sling3.png');
         //Sets the variable. 
         this.pointB = pointB;
         //Creates the constraint.
